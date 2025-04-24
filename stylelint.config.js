2bd2bcaf4a3b5892083311bfa30a0528e8da8c57
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-04-24 17:43:07 +0200

    fix: Build

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,17 @@
+import propertyGroups from "stylelint-config-recess-order/groups";
+
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ["stylelint-config-standard-scss", "stylelint-config-recess-order"],
+  extends: ["stylelint-config-standard-scss"],
   plugins: ["stylelint-order"],
   rules: {
+    // Handled through order plugin.
+    "declaration-empty-line-before": null,
+    "order/properties-order": propertyGroups.map(group => ({
+      ...group,
+      emptyLineBefore: "always",
+      noEmptyLineBetween: true,
+    })),
     "selector-class-pattern": null,
   },
 };
