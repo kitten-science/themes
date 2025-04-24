6af424a9ca6b9bfaf3357ecfa32ad7e80245c242
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-04-24 18:35:12 +0200

    fix: Clean game roots

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,7 @@ import propertyGroups from "stylelint-config-recess-order/groups";
 
 /** @type {import('stylelint').Config} */
 export default {
+  ignoreFiles: ["node_modules", "source/theme-reset.css"],
   extends: ["stylelint-config-standard-scss", "@stylistic/stylelint-config"],
   plugins: ["@stylistic/stylelint-plugin", "stylelint-order"],
   rules: {
