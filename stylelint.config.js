dc246c5e93b4d1a68d95c65eecca83259928f722
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-04-27 10:22:12 +0200

    style: Switch back to clean order

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,19 +1,14 @@
-import propertyGroups from "stylelint-config-recess-order/groups";
-
 /** @type {import('stylelint').Config} */
 export default {
   ignoreFiles: ["node_modules", "source/theme-reset.css"],
-  extends: ["stylelint-config-standard-scss", "@stylistic/stylelint-config"],
-  plugins: ["@stylistic/stylelint-plugin", "stylelint-order", "stylelint-use-nesting"],
+  extends: [
+    "stylelint-config-standard-scss",
+    "@stylistic/stylelint-config",
+    "stylelint-config-clean-order",
+  ],
+  plugins: ["@stylistic/stylelint-plugin", "stylelint-use-nesting"],
   rules: {
     "csstools/use-nesting": "always",
-    // Handled through order plugin.
-    "declaration-empty-line-before": null,
-    "order/properties-order": propertyGroups.map(group => ({
-      ...group,
-      emptyLineBefore: "always",
-      noEmptyLineBetween: true,
-    })),
     "selector-class-pattern": null,
     "selector-id-pattern": null,
   },
