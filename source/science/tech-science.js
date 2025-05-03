ae52eec04b9ce7f90b222fea4644a478b307b62e
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-05-04 01:13:08 +0200

    feat: More better

--- source/science/tech-science.js
+++ source/science/tech-science.js
@@ -20,19 +20,19 @@ const OrphanedItems = ["unobtainiumAxe", "unobtainiumSaw"];
 const buffer = [];
 let indentation = 0;
 
-buffer.push("digraph kittens {");
+const render = _ => buffer.push(indent(_, indentation));
+const renderImplied = (a, b) => render(`${a} -> ${b} [style="dotted";];`);
+
+render("digraph kittens {");
 ++indentation;
 
-buffer.push(indent(`fontname="sans-serif";`, indentation));
-buffer.push(indent(`pad="0.5";`, indentation));
-buffer.push(indent(`rankdir="LR";`, indentation));
+render(`fontname="sans-serif";`);
+render(`pad="0.5";`);
+render(`rankdir="LR";`);
 
 // Buildings
-buffer.push(
-  indent(
-    `node [fillcolor="#f2d174"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
-    indentation,
-  ),
+render(
+  `node [fillcolor="#f2d174"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
 );
 for (const _ of Object.values(buildings)) {
   if (OrphanedItems.includes(_.name)) {
@@ -40,146 +40,116 @@ for (const _ of Object.values(buildings)) {
   }
 
   if (_.stages) {
-    buffer.push(
-      indent(
-        `${_.name} [label="${_.stages[0].label}"; tooltip="ID: ${_.name}:0\\n${renderPrices(_.stages[0].prices)}";];`,
-        indentation,
-      ),
+    render(
+      `${_.name} [label="${_.stages[0].label}"; tooltip="ID: ${_.name}:0\\n${renderPrices(_.stages[0].prices)}";];`,
     );
-    buffer.push(
-      indent(
-        `${_.name}1 [label="${_.stages[1].label}"; tooltip="ID: ${_.name}:1\\n${renderPrices(_.stages[1].prices)}";];`,
-        indentation,
-      ),
+    render(
+      `${_.name}1 [label="${_.stages[1].label}"; tooltip="ID: ${_.name}:1\\n${renderPrices(_.stages[1].prices)}";];`,
     );
     continue;
   }
 
-  buffer.push(
-    indent(
-      `${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`,
-      indentation,
-    ),
-  );
+  render(`${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`);
 }
 
 // Void Space Upgrades
-buffer.push(
-  indent(
-    `node [fillcolor="#eb9de4"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
-    indentation,
-  ),
+render(
+  `node [fillcolor="#eb9de4"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
 );
 for (const _ of Object.values(voidspaceUpgrades)) {
   if (OrphanedItems.includes(_.name)) {
     continue;
   }
 
-  buffer.push(
-    indent(
-      `${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`,
-      indentation,
-    ),
-  );
+  render(`${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`);
 }
 
 // Chronoforge Upgrades
-buffer.push(
-  indent(
-    `node [fillcolor="#eb9de4"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
-    indentation,
-  ),
+render(
+  `node [fillcolor="#eb9de4"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
 );
 for (const _ of Object.values(chronoforgeUpgrades)) {
   if (OrphanedItems.includes(_.name)) {
     continue;
   }
 
-  buffer.push(
-    indent(
-      `${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`,
-      indentation,
-    ),
-  );
+  render(`${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`);
 }
 
 // Ziggurat Upgrades
-buffer.push(
-  indent(
-    `node [fillcolor="#f7f065"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
-    indentation,
-  ),
+render(
+  `node [fillcolor="#f7f065"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
 );
 for (const _ of Object.values(zigguratUpgrades)) {
   if (OrphanedItems.includes(_.name)) {
     continue;
   }
 
-  buffer.push(
-    indent(
-      `${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`,
-      indentation,
-    ),
-  );
+  render(`${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`);
 }
 
 // Transcendence Upgrades
-buffer.push(
-  indent(
-    `node [fillcolor="#f7f065"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
-    indentation,
-  ),
+render(
+  `node [fillcolor="#f7f065"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
 );
 for (const _ of Object.values(transcendenceUpgrades)) {
   if (OrphanedItems.includes(_.name)) {
     continue;
   }
 
-  buffer.push(
-    indent(
-      `${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`,
-      indentation,
-    ),
-  );
+  render(`${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`);
 }
 
 // Religion Upgrades
-buffer.push(
-  indent(
-    `node [fillcolor="#f7f065"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
-    indentation,
-  ),
+render(
+  `node [fillcolor="#f7f065"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
 );
 for (const _ of Object.values(religionUpgrades)) {
   if (OrphanedItems.includes(_.name)) {
     continue;
   }
 
-  buffer.push(
-    indent(
-      `${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`,
-      indentation,
-    ),
-  );
+  render(`${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`);
 }
 
 // Space Programs
-buffer.push(
-  indent(
-    `node [fillcolor="#9ef0a5"; fontcolor="#000000"; fontname="sans-serif"; shape="box"; style="filled";];`,
-    indentation,
-  ),
+render(
+  `node [fillcolor="#9ef0a5"; fontcolor="#000000"; fontname="sans-serif"; shape="box"; style="filled";];`,
 );
 for (const _ of Object.values(programs)) {
   if (OrphanedItems.includes(_.name)) {
     continue;
   }
 
-  buffer.push(
-    indent(
-      `${_.name} [label="  ${_.label}  "; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`,
-      indentation,
-    ),
+  render(
+    `${_.name} [label="  ${_.label}  "; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`,
+  );
+}
+
+// Upgrades
+render(
+  `node [fillcolor="#93ccc6"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
+);
+for (const _ of Object.values(upgrades)) {
+  if (OrphanedItems.includes(_.name)) {
+    continue;
+  }
+
+  render(`${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`);
+}
+
+// Techs
+render(
+  `node [fillcolor="#465666"; fontcolor="#ffffff"; fontname="sans-serif"; shape="box"; style="filled";];`,
+);
+for (const _ of Object.values(techs)) {
+  if (OrphanedItems.includes(_.name)) {
+    continue;
+  }
+
+  // HACK: Add some spaces around the label to aid with padding/readability.
+  render(
+    `${_.name} [label="   ${_.label}  "; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`,
   );
 }
 
@@ -189,11 +159,8 @@ for (const _ of Object.values(planets)) {
     continue;
   }
 
-  buffer.push(
-    indent(
-      `${_.name} [label="${_.label}"; tooltip="ID: ${_.name}"; fillcolor="#ffffff"; fontcolor="#000000"; fontname="sans-serif"; shape="circle"; style="filled";];`,
-      indentation,
-    ),
+  render(
+    `${_.name} [label="${_.label}"; tooltip="ID: ${_.name}"; fillcolor="#ffffff"; fontcolor="#000000"; fontname="sans-serif"; shape="circle"; style="filled";];`,
   );
 
   buffer.push(
@@ -202,130 +169,127 @@ for (const _ of Object.values(planets)) {
       indentation,
     ),
   );
-  for (const k of _.buildings) {
-    buffer.push(indent(`${k.name} [label="${k.label}"; tooltip="ID: ${k.name}";];`, indentation));
-    buffer.push(indent(`${_.name} -> ${k.name};`, indentation));
+  for (const building of _.buildings) {
+    render(
+      `${building.name} [label="${building.label}"; tooltip="ID: ${building.name}\\n${renderPrices(building.prices)}";];`,
+    );
+    render(`${_.name} -> ${building.name};`);
+
+    for (const tech of Array.isArray(building.requiredTech) ? building.requiredTech : []) {
+      render(`${tech} -> ${building.name};`);
+    }
   }
-}
-
-// Upgrades
-buffer.push(
-  indent(
-    `node [fillcolor="#93ccc6"; fontcolor="#000000"; fontname="sans-serif"; shape="oval"; style="filled";];`,
-    indentation,
-  ),
-);
-for (const _ of Object.values(upgrades)) {
-  if (OrphanedItems.includes(_.name)) {
-    continue;
-  }
-
-  buffer.push(
-    indent(
-      `${_.name} [label="${_.label}"; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`,
-      indentation,
-    ),
-  );
-}
-
-// Techs
-buffer.push(
-  indent(
-    `node [fillcolor="#465666"; fontcolor="#ffffff"; fontname="sans-serif"; shape="box"; style="filled";];`,
-    indentation,
-  ),
-);
-for (const _ of Object.values(techs)) {
-  if (OrphanedItems.includes(_.name)) {
-    continue;
-  }
-
-  // HACK: Add some spaces around the label to aid with padding/readability.
-  buffer.push(
-    indent(
-      `${_.name} [label="   ${_.label}  "; tooltip="ID: ${_.name}\\n${renderPrices(_.prices)}";];`,
-      indentation,
-    ),
-  );
 }
 
 for (const _ of [
+  ...Object.values(buildings),
+  ...Object.values(chronoforgeUpgrades),
+  ...Object.values(planets),
+  ...Object.values(programs),
+  ...Object.values(religionUpgrades),
   ...Object.values(techs),
+  ...Object.values(transcendenceUpgrades),
   ...Object.values(upgrades),
   ...Object.values(voidspaceUpgrades),
-  ...Object.values(chronoforgeUpgrades),
-  ...Object.values(religionUpgrades),
-  ...Object.values(buildings),
-  ...Object.values(programs),
-  ...Object.values(planets),
   ...Object.values(zigguratUpgrades),
-  ...Object.values(transcendenceUpgrades),
 ]) {
   for (const unlock of [
-    ...(_.unlocks?.tech ?? []),
-    ...(_.unlocks?.upgrades ?? []),
-    ...(_.unlocks?.voidSpace ?? []),
+    ...(_.unlocks?.buildings ?? []),
     ...(_.unlocks?.chronoforge ?? []),
-    ...(_.unlocks?.zigguratUpgrades ?? []),
-    ...(_.unlocks?.transcendenceUpgrades ?? []),
     ...(_.unlocks?.planet ?? []),
     ...(_.unlocks?.spaceMission ?? []),
-    ...(_.unlocks?.buildings ?? []),
+    ...(_.unlocks?.tech ?? []),
+    ...(_.unlocks?.transcendenceUpgrades ?? []),
+    ...(_.unlocks?.upgrades ?? []),
+    ...(_.unlocks?.voidSpace ?? []),
+    ...(_.unlocks?.zigguratUpgrades ?? []),
   ]) {
-    buffer.push(indent(`${_.name} -> ${unlock};`, indentation));
+    render(`${_.name} -> ${unlock};`);
   }
   for (const unlock of [...(_.unlocks?.stages ?? [])]) {
-    buffer.push(indent(`${_.name} -> ${unlock.bld}1;`, indentation));
+    render(`${_.name} -> ${unlock.bld}1;`);
   }
 }
+
+// Perks
+render(
+  `megalomenia [label="   Megalomenia   "; tooltip="ID: megalomenia"; fillcolor="#afffff"; fontcolor="#000000"; fontname="sans-serif"; shape="rect"; style="dashed";];`,
+);
+render("megalomenia -> marker;");
+render("megalomenia -> blackPyramid;");
 
 // Fill implicit unlocks.
-buffer.push(indent("library -> calendar;", indentation));
-buffer.push(indent("calendar -> temporalImpedance;", indentation));
-buffer.push(indent("chronoforge -> blastFurnace;", indentation));
-buffer.push(indent("chronoforge -> temporalBattery;", indentation));
-buffer.push(indent("chronoforge -> temporalAccelerator;", indentation));
-buffer.push(indent("workshop -> mineralHoes;", indentation));
-buffer.push(indent("workshop -> ironHoes;", indentation));
-buffer.push(indent("workshop -> mineralAxes;", indentation));
-buffer.push(indent("workshop -> ironAxes;", indentation));
-buffer.push(indent("workshop -> stoneBarns;", indentation));
-buffer.push(indent("workshop -> reinforcedBarns;", indentation));
-buffer.push(indent("cryochambers -> usedCryochambers;", indentation));
-buffer.push(indent("unicornPasture -> unicornTomb;", indentation));
-buffer.push(indent("ziggurat -> unicornTomb;", indentation));
-buffer.push(indent("unicornPasture -> unicornGraveyard;", indentation));
-buffer.push(indent("ziggurat -> unicornGraveyard;", indentation));
-buffer.push(indent("temple -> solarchant;", indentation));
-buffer.push(indent("temple -> blackPyramid;", indentation));
-buffer.push(indent("temple -> marker;", indentation));
-buffer.push(indent("transcendence -> blackObelisk;", indentation));
-buffer.push(indent("dimensionalPhysics -> artificialGravity;", indentation));
-buffer.push(indent("rocketry -> orbitalLaunch;", indentation));
+render("library -> calendar;");
+render("chronoforge -> blastFurnace;");
+render("chronoforge -> temporalBattery;");
+render("chronoforge -> temporalAccelerator;");
+// Some stuff is pre-unlocked in workshop.
+render("workshop -> mineralHoes;");
+render("workshop -> ironHoes;");
+render("workshop -> mineralAxes;");
+render("workshop -> ironAxes;");
+render("workshop -> stoneBarns;");
+render("workshop -> reinforcedBarns;");
+// Makes sense, right?
+renderImplied("cryochambers", "usedCryochambers");
+render("unicornPasture -> unicornTomb;");
+// Ziggurat unlocks Ziggurats stuff.
+render("ziggurat -> unicornTomb;");
+render("unicornPasture -> unicornGraveyard;");
+render("ziggurat -> unicornGraveyard;");
+// No sorrow, no pyramid.
+renderImplied("unicornPasture", "blackPyramid");
+// No unicorn tears, no marker.
+renderImplied("unicornPasture", "marker");
+// No unobtainium, no pyramid/marker.
+renderImplied("moonOutpost", "blackPyramid");
+renderImplied("moonOutpost", "marker");
+// No religion tab, no unicorn stuff.
+render("theology -> blackPyramid;");
+render("theology -> marker;");
+// ?
+render("dimensionalPhysics -> artificialGravity;");
+// No rockets, no launch.
+render("rocketry -> orbitalLaunch;");
+// Need necrocorns, thus marker.
+renderImplied("marker", "unicornGraveyard");
+renderImplied("marker", "unicornNecropolis");
+renderImplied("marker", "mausoleum");
+// Needs void, thus Chronosphere.
+renderImplied("chronosphere", "darkNova");
+renderImplied("chronosphere", "mausoleum");
+renderImplied("chronosphere", "holyGenocide");
+// Need gold, so Smelter.
+renderImplied("smelter", "goldenSpire");
+renderImplied("smelter", "sunAltar");
+renderImplied("smelter", "stainedGlass");
+renderImplied("smelter", "solarRevolution");
+renderImplied("smelter", "basilica");
+renderImplied("smelter", "templars");
+renderImplied("smelter", "apocripha");
+renderImplied("smelter", "transcendence");
+// No unobtainium, no crazy space stuff.
+renderImplied("moonOutpost", "spaceElevator");
+renderImplied("moonOutpost", "moonBase");
+renderImplied("moonOutpost", "hydroponics");
+// Probably need to register all upgrades...
+renderImplied("spaceBeacon", "relicStation");
+// No pyramid, no elders, no relics.
+renderImplied("blackPyramid", "cryptotheology");
+// No pyramid, no elders, no TCs.
+renderImplied("blackPyramid", "temporalImpedance");
 
-let previous = undefined;
 for (const _ of Object.values(transcendenceUpgrades).sort((a, b) => a.tier - b.tier)) {
-  if (!previous) {
-    previous = _;
-    continue;
-  }
-  buffer.push(indent(`${previous.name} -> ${_.name};`, indentation));
-  buffer.push(indent(`temple -> ${_.name} [style="invis";];`, indentation));
-  previous = _;
+  renderImplied("transcendence", _.name);
+  // No pyramid, no elders, no relics.
+  renderImplied("blackPyramid", _.name);
 }
 
-previous = undefined;
 for (const _ of Object.values(religionUpgrades).sort((a, b) => a.faith - b.faith)) {
-  if (!previous) {
-    previous = _;
-    continue;
-  }
-  buffer.push(indent(`${previous.name} -> ${_.name};`, indentation));
-  buffer.push(indent(`temple -> ${_.name} [style="invis";];`, indentation));
-  previous = _;
+  render(`theology -> ${_.name};`);
 }
 
-buffer.push("}");
+render("}");
 --indentation;
 
 // In case you want to copy to another DOT renderer, like for PNG exporting.
